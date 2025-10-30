73f452ee51311811552bc917635eb5845a00e3ba
Vijay <vijaykarthik223344@gmail.com>
2025-10-30 07:55:29 +0530

    Correct syntax and remove duplicate outputs in v.java
    Fix syntax errors and duplicate print statements in v.java.

--- v.java
+++ v.java
@@ -2,4 +2,9 @@ public class{
 }
 public Static void main(){
 System.out.println("vijay karthik");
+  System.out.println("vijay karthik");
+  System.out.println("vijay karthik"):
+    System.out.println("vijay karthik");
+  
+  
 }
